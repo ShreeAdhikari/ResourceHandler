4e54ec68b5fe0ecdae2fa46312c25c37e91cb9e3
ShreeAdhikari <165574457+ShreeAdhikari@users.noreply.github.com>
2026-08-26 19:36:05 +0545

    server.js second

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("NTC Equipment Booking System API is running!");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
